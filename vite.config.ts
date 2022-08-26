dd18a06c78ebbda61badeb42ad907264334ec171
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-08-26 19:32:05 -0300

    config: initial configurations

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, './src')},
+      { find: "@animations", replacement: path.resolve(__dirname, './src/animations')},
+      { find: "@components", replacement: path.resolve(__dirname, './src/components')},
+      { find: "@contexts", replacement: path.resolve(__dirname, './src/contexts')},
+      { find: "@pages", replacement: path.resolve(__dirname, './src/pages')},
+      { find: "@images", replacement: path.resolve(__dirname, '/images')}
+    ]
+  }
+})
