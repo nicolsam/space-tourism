3ce720daa5e4991333fd114dda03ac7bbf67fba9
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-08-30 21:52:38 -0300

    add: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import Menu from '@components/Menu';
+
+const Header = () => {
+  return (
+    <div className="w-screen relative xl:py-10">
+        <Menu />
+    </div>
+  )
+}
+
+export default Header
