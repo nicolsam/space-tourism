4e1391b80d4b099df85080ad55e8cc8b88d999a9
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-02 17:18:52 -0300

    add: icon and row

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom';
 import Menu from '@components/Menu';
+
+import Logo from '@icons/shared/logo.svg';
+
+import * as S from './styles';
 
 const Header = () => {
   return (
-    <div className="w-screen relative xl:py-10">
+    <div className="h-fit xl:h-1/6 relative top-0 z-50 flex flex-row xl:justify-end items-center xl:items-center">
+        <div className="w-2/5 h-full pl-12 xl:hidden">
+          <Link to="/" className="h-fit cursor-pointer">
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </div>
+
+        <div className="hidden xl:block w-2/5 z-50">
+          <S.Row />
+        </div>
+
         <Menu />
     </div>
   )
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Row = styled.div`
+  width: 105%;
+  height: .1rem;
+
+  background: #494A54;
+`;
