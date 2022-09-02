e38e0910f8c97ef0aa2ca847aaffd0da737bd576
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-02 17:19:59 -0300

    add: Home Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom';
+import * as S from './styles';
+
 import './index.css';
 
 const Home = () => {
     return (
-        <div>
-            <p>hello</p>
+        <div className="h-5/6 xl:pr-52 xl:py-40 relative flex flex-col gap-36 xl:gap-0 xl:flex-row justify-center items-center xl:items-end">
+            <div className="w-3/5 xl:w-2/5 text-white flex flex-col justify-end">
+                <h1 className="w-full flex flex-col">
+                    <S.Travel className="uppercase xl:text-4xl tracking-widest self-center xl:self-start">so, you want travel to</S.Travel>
+                    <S.Space className="uppercase self-center">space</S.Space>
+                </h1>
+                <S.Description className="text-lg xl:text-xl self-center xl:self-start text-center xl:text-left">
+                    Let’s face it; if you want to go to space, 
+                    you might as well genuinely go to outer space and not hover kind of on the edge of it. 
+                    Well sit back, and relax because we’ll give you a truly out of this world experience!
+                </S.Description>
+            </div>
+            <div className="w-3/5 flex justify-center items-center xl:justify-end xl:items-end">
+                <S.Explore className="w-60 h-60 xl:w-64 xl:h-64 2xl:w-72 2xl:h-72 rounded-full">
+                    <Link to="/destination" className="w-full h-full bg-white rounded-full cursor-pointer flex justify-center items-center">
+                        <S.ExploreText className="uppercase text-3xl tracking-widest">explore</S.ExploreText>
+                    </Link>
+                </S.Explore>
+            </div>
         </div>
     );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Travel = styled.span`
+  font-family: 'Barlow Condensed', sans-serif;
+`;
+
+export const Space = styled.span`
+  font-family: 'Bellefair', sans-serif;
+  
+  font-size: 11rem;
+
+  @media (max-width: 1280px) {
+    font-size: 9.375rem; /* 150px */
+  }
+`;
+
+export const Description = styled.p`
+  font-family: 'Barlow', sans-serif;
+`;
+
+export const Explore = styled.h2`
+
+  transition: outline .3s;
+  outline: 0 solid rgba(255, 255, 255, .1);
+
+  &:hover {
+    transition: outline .3s;
+    outline-width: 5.3rem;
+  }
+`;
+
+export const ExploreText = styled.h2`
+  font-family: 'Bellefair', sans-serif;
+`;
