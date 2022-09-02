d12798c14d9dca33a913394099fce8af56ee0b21
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-02 17:20:40 -0300

    style: icon for desktop version

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Header from '@components/Header';
 import Home from '@pages/Home';
 
+import Logo from '@icons/shared/logo.svg';
+
 function App() {
 
   return (
-    <div className="App">
+    <div className="App flex flex-row">
 
-      <Header />
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes> 
+      <div className="hidden xl:block w-52 h-screen">
+        <div className="h-1/6 flex justify-center items-center">
+          <Link to="/" className="h-fit cursor-pointer">
+            <img src={Logo} alt="Logo" />
+          </Link>
+        </div>
+      </div>
+
+      <main className="h-screen xl:h-auto">
+        <Header />
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes> 
+      </main>
+
     </div>
   )
 }
