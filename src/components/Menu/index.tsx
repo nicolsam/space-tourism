2e1d6599fbcaa02a51dc8a35a643fea60c37f54c
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-05 21:15:15 -0300

    fix: current active menu item

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,18 +1,26 @@
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Item from './Item';
 
+import * as data from '@assets/data.json';
 import * as S from './styles';
 
 const Menu = () => {
 
     const location = useLocation();
+    const [path, setPath] = useState('/');
+
+    useEffect(() => {
+        let pathname = location.pathname.split('/');
+        setPath('/' + pathname[1]);
+    }, [location.pathname]);
 
     return (
-        <div className="w-3/5 pr-20 2xl:pr-52 bg-white/[.04] backdrop-blur-2xl flex gap-5 xl:gap-14 flex-row justify-center xl:justify-end">
-            <Item active={location.pathname} link="/"><S.MText>00</S.MText>HOME</Item>  
-            <Item active={location.pathname} link="/destination"><S.MText>01</S.MText>DESTINATION</Item>  
-            <Item active={location.pathname} link="/crew"><S.MText>02</S.MText>CREW</Item>  
-            <Item active={location.pathname} link="/technology"><S.MText>03</S.MText>TECHNOLOGY</Item>  
+        <div className="w-3/5 2xl:pr-52 bg-white/[.04] backdrop-blur-2xl flex gap-5 xl:gap-14 flex-row justify-center xl:justify-end">
+            <Item active={path} link="/"><S.MText>00</S.MText>HOME</Item>  
+            <Item active={path} link="/destination" sublink={data.destinations[0].name.toLowerCase()}><S.MText>01</S.MText>DESTINATION</Item>  
+            <Item active={path} link="/crew"><S.MText>02</S.MText>CREW</Item>  
+            <Item active={path} link="/technology"><S.MText>03</S.MText>TECHNOLOGY</Item>  
         </div>
     );
 }
