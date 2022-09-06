1509d9ba245ce75597fb5a5f9f5c383014bdb845
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-05 21:14:32 -0300

    feat: sublink used to redirect page

--- src/components/Menu/Item/index.tsx
+++ src/components/Menu/Item/index.tsx
@@ -4,13 +4,23 @@ import * as S from './styles';
 interface ItemProps {
     active: string;
     link: string;
+    sublink?: string;
     children: React.ReactNode;
 }
 
-const Item = ({active, link, children}:ItemProps) => {
+const Item = ({active, link, sublink, children}:ItemProps) => {
+
+    const handleLink = () => {
+        if(!sublink || sublink == undefined) {
+            return link;
+        }
+
+        return `${link}/${sublink}`;
+    }
+
     return (
         <S.Title active={active === link}>
-            <Link to={link} className="h-28 flex flex-col justify-center items-center hover:cursor-pointer">
+            <Link to={handleLink()} className="h-28 flex flex-col justify-center items-center hover:cursor-pointer">
                 <p className="text-sm xl:text-base 2xl:text-xl text-white tracking-widest flex flex-row gap-3">{children}</p>
             </Link>
         </S.Title>
