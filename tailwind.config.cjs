66b9d505b1825885c5192a0a1c0d4624dea27c6b
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-05 21:12:50 -0300

    config: font families and custom gray color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,13 +5,21 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'barlowcondensed': ['"Barlow Condensed"', 'sans-serif', '"Open Sans"'],
+      'barlow': ['Barlow', 'sans-serif', '"Open Sans"'],
+      'bellefair': ['Bellefair', 'sans-serif', '"Open Sans"'],
+    },
     letterSpacing: {
       widest: '.23em'
     },
     extend: {
       blur: {
         super: '81.55px'
-      }
+      },
+      colors: {
+        'cgray': '#D0D6F9'
+      },
     },
   },
   plugins: [],
