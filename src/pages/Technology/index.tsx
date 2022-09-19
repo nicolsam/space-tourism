07a17e9442c4c9105ab0afedadd602bd9730a642
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-19 15:41:44 -0300

    add: technology page

---
+++ src/pages/Technology/index.tsx
@@ -0,0 +1,12 @@
+
+const Technology = () => {
+    return (
+        <div className="h-5/6 pt-8 flex flex-col gap-12 xl:gap-24 justify-center items-center">
+            <div className="h-fit pl-12 xl:pl-0 xl:w-full uppercase self-start">
+            <h1 className="font-barlowcondensed text-white text-xl xl:text-3xl tracking-widest flex flex-row gap-8"><span className="text-white/25 font-bold">03</span>space launch 101</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Technology;
