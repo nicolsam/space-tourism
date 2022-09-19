b3a6a2446e131def3e5ec77d9e75551bb1262e09
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-19 15:40:23 -0300

    style: better responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import { Link, useLocation } from 'react-router-dom';
 import { GlobalStyle } from './global-styles';
 import * as data from '@assets/data.json';
 
+import { convertStringToSlug } from '@utils/string-utils';
+
 import Header from '@components/Header';
 import Home from '@pages/Home';
 import Destination from '@pages/Destination';
+import Crew from '@pages/Crew';
+import Technology from '@pages/Technology';
 
 import Logo from '@icons/shared/logo.svg';
 
@@ -27,17 +31,24 @@ function App() {
         </div>
       </div>
 
-      <main className="w-full xl:w-11/12 h-screen xl:h-auto">
+      <main className="w-full xl:w-11/12 overflow-hidden h-screen xl:h-auto">
         <Header />
 
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/destination" element={<Destination destination={data.destinations[0]} />} />
-            
-            {data.destinations.map((destination) => (
-              <Route path={`/destination/${destination.name.toLowerCase()}`} element={<Destination destination={destination} />} />
-            ))}
-        </Routes> 
+        <div className="h-full xl:h-5/6 xl:pr-24 2xl:pr-28 3xl:pr-52">
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/destination" element={<Destination destination={data.destinations[0]} />} />
+              
+              {data.destinations.map((destination) => (
+                <Route path={`/destination/${destination.name.toLowerCase()}`} element={<Destination destination={destination} />} />
+              ))}
+
+              {data.crew.map((crewmate) => (
+                <Route path={`/crew/${convertStringToSlug(crewmate.name)}`} element={<Crew crewmate={crewmate} />} />
+              ))}
+
+          </Routes> 
+        </div>
       </main>
 
     </div>
