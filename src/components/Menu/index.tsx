4be37d5e625430bc22492ae9f1e4a3783c744264
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2022-09-20 12:08:26 -0300

    add: technology sublink

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,6 +8,8 @@ import * as data from '@assets/data.json';
 import * as S from './styles';
 import hamburgerIcon from '@icons/shared/icon-hamburger.svg';
 
+import { convertStringToSlug } from '@utils/string-utils';
+
 import { IOptions } from '@shared/interfaces/options.interface';
 
 const optionsInitialValue:IOptions[] = [
@@ -77,11 +79,11 @@ const Menu = () => {
 
     return (
         <>
-            <div className="w-3/5 xl:pr-24 2xl:pr-28 3xl:pr-52 bg-white/[.04] backdrop-blur-2xl hidden xl:flex gap-5 xl:gap-14 flex-row justify-center xl:justify-end">
+            <div className="w-3/5 xl:pr-24 2xl:pr-28 3xl:pr-52 bg-white/[.04] backdrop-blur-2xl hidden md:flex gap-5 xl:gap-14 flex-row justify-center xl:justify-end">
                 <Item active={path} link="/"><S.MText>00</S.MText>HOME</Item>  
                 <Item active={path} link="/destination" sublink={data.destinations[0].name.toLowerCase()}><S.MText>01</S.MText>DESTINATION</Item>  
                 <Item active={path} link="/crew" sublink="douglas-hurley"><S.MText>02</S.MText>CREW</Item>  
-                <Item active={path} link="/technology"><S.MText>03</S.MText>TECHNOLOGY</Item>  
+                <Item active={path} link="/technology" sublink={convertStringToSlug(data.technology[0].name)}><S.MText>03</S.MText>TECHNOLOGY</Item>  
             </div>
 
             <img
